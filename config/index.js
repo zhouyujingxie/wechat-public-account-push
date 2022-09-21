c4c52ecc842327439630a920e5118afcf9bed817
zhouyu <91958989+zhouyujingxie@users.noreply.github.com>
2022-09-21 13:52:57 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "op6D16BpNHaJlroyBPx2GsB42G8U",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "T14YgwK07IV8fMqH86-Fvi1jg7AkFGlVsdBC1ZM0Q64",
+      useTemplateId: "	p5ISSn37tOeWjZxHtagqO0hQT4EcNn9l7BV_Lh44qLg",
       // 所在省份
       province: "山西",
       // 所在城市
@@ -74,15 +74,15 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "https://s.weibo.com/top/summary?Refer=top_hot&topnav=1&wvr=6",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {"type": "*生日", "name": "老婆", "year": "1996", "date": "09-09"},
-        {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+        {"type": "*生日", "name": "冯凯", "year": "1996", "date": "09-09"},
+        {"type": "*生日", "name": "冯凯", "year": "1996", "date": "09-09"},
+        {"type": "*生日", "name": "冯凯", "year": "1996", "date": "09-09"},
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-        {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+      
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
